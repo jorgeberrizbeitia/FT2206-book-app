03c729014a4cf06dbfd7a814f03b1b9804db85cd
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2022-07-25 13:05:32 +0200

    C done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,20 +7,34 @@ router.get("/", (req, res, next) => {
 });
 
 // GET "/books" => Listar todos los titulos de los libros
-router.get("/books", (req, res, next) => {
+// router.get("/books", (req, res, next) => {
 
-  // 1 Obtener los datos de los libros (solo titulos) como promesa
-  Book.find().select("title")
-  .then((listOfBooks) => {
+//   // 1 Obtener los datos de los libros (solo titulos) como promesa
+//   Book.find().select("title")
+//   .then((listOfBooks) => {
+//     // 2. renderizamos una vista hbs y le pasamos los libros
+//     res.render("book/list.hbs", {
+//       listOfBooks
+//     })
+//   })
+//   .catch((err) => {
+//     next(err)
+//   })
+//   // 3. visualizar los libros en la vista
+// })
+
+// GET "/books" => Listar todos los titulos de los libros
+router.get("/books", async (req, res, next) => {
+  // EJEMPLO CON ASYNC/AWAIT
+  try {
+    // 1 Obtener los datos de los libros (solo titulos) como promesa
+    const listOfBooks = await Book.find().select("title")
     // 2. renderizamos una vista hbs y le pasamos los libros
-    res.render("book/list.hbs", {
-      listOfBooks
-    })
-  })
-  .catch((err) => {
+    res.render("book/list.hbs", { listOfBooks})
+    // 3. visualizar los libros en la vista
+  } catch (err) {
     next(err)
-  })
-  // 3. visualizar los libros en la vista
+  }
 })
 
 // GET "/books/:id/details" => Ver los detalles de un libro por su id
@@ -43,6 +57,33 @@ router.get("/books/:bookId/details", (req, res, next) => {
   }).catch((err) => {
     next(err)
   })
+})
+
+// GET "/books/create" => Renderizar el formulario de crear libro
+router.get("/books/create", (req, res, next) => {
+  res.render("book/add-form.hbs")
+})
+
+// POST "/books/create" => recibir detalles de un libro y crearlo en la BD
+router.post("/books/create", (req, res, next) => {
+  // 1. es donde recibimos la data
+  console.log("Accediendo a la RUTA!")
+  console.log(req.body)
+  const {title, description, author} = req.body
+  // 2. creamos el libro en la DB
+  Book.create({
+    title,
+    description,
+    author
+  })
+  .then(() => {
+    console.log("Libro creado")
+    res.redirect("/books")
+  })
+  .catch((err) => {
+    next(err)
+  })
+
 })
 
 module.exports = router;
